de45cbbcddb7535dfded5cf5825e4021374fc362
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-08 00:48:45 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -5,16 +5,23 @@ class OwnersController < ApplicationController
     erb :'/owners/index' 
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -23,7 +30,15 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    
+    @owner = Owner.find_by_id(params[:id])
+    @owner.update(params[:owner])
+    
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}"
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,18 +9,35 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+  post '/pets' do
+    if params[:owner][:name].empty? #if no name was placed #choosing from previous owners
+      @owner = Owner.find_by_id(params[:pet][:owner_id])
+      @pet = Pet.create(name: params[:pet][:name])
+      @owner.pets << @pet
+    
+    else #If a name was placed #new owner being created # new owner works well
+      @owner = Owner.create(name: params[:owner][:name])
+      @pet = Pet.create(name: params[:pet][:name])
+      @owner.pets << @pet
+    end
+    redirect to "/pets/#{@pet.id}"
   end
-
-  get '/pets/:id' do 
+  
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+  
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
-  patch '/pets/:id' do 
-
+  post '/pets/:id' do 
+    @pet = Pet.find_by_id(params[:id])
+    @owner = Owner.find_by_id(params[:id])
+    @pet.update(params[:pet])
+    @owner.update(params[:owner])
     redirect to "pets/#{@pet.id}"
   end
 end
