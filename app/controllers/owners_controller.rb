5033b0b4cf1f2ae8c4ff36264c2e1eb7c5ade395
LucyWeltner <lucyiweltner@gmail.com>
2020-01-09 01:56:23 -0500

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,15 +6,26 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    if params[:pet][:name].length > 0 
+      new_pet = Pet.create(name: params[:pet][:name])
+      if params[:owner][:pet_ids]
+        params[:owner][:pet_ids] << new_pet.id
+      else 
+        params[:owner][:pet_ids] = ["#{new_pet.id}"]
+      end
+    end
+    @owner = Owner.create(params[:owner])
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -24,6 +35,14 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do 
-   
+    @owner = Owner.find(params[:id])
+    if !params[:pet][:name].empty? && params[:owner][:pet_ids]
+      params[:owner][:pet_ids] << Pet.create(params[:pet]).id
+    end 
+    if !params[:pet][:name].empty? && !params[:owner][:pet_ids]
+      params[:owner][:pet_ids] = ["#{Pet.create(params[:pet]).id}"]
+    end
+    @owner.update(params[:owner])
+    erb :'owners/show'
   end
 end
